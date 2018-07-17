a3af07129efaacac4fb51d1ff668b634206249a5
Stephen Warton <stephen.warton@gmail.com>
2018-07-16 23:19:42 -0400

    updated to 8.13.1

--- api/router.js
+++ api/router.js
@@ -8,20 +8,30 @@ const router = express.Router();
 
 router.post('/mastery', (req, res, next) => {
   let summonerName = req.body.name;
-  if(valid.user(summonerName)){
-    axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=${process.env.API_KEY}`)
-      .then(function (response) {
+  if (valid.user(summonerName)) {
+    axios
+      .get(
+        `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=${
+          process.env.API_KEY
+        }`
+      )
+      .then(function(response) {
         let user = {
           name: response.data.name,
           icon: response.data.profileIconId,
           id: response.data.id
         };
-        axios.get(`https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/${user.id}?api_key=${process.env.API_KEY}`)
-          .then(function (response) {
+        axios
+          .get(
+            `https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/${
+              user.id
+            }?api_key=${process.env.API_KEY}`
+          )
+          .then(function(response) {
             let masteries = response.data;
             let points = 0;
             let masteryLevel = 0;
-            for(let object of masteries){
+            for (let object of masteries) {
               let champion = champions[object.championId];
               object.key = champion.key;
               object.name = champion.name;
@@ -39,12 +49,12 @@ router.post('/mastery', (req, res, next) => {
             };
             res.json(data);
           })
-          .catch(function (error) {
+          .catch(function(error) {
             //console.log(error);
             next(new Error(error));
           });
       })
-      .catch(function (error) {
+      .catch(function(error) {
         //console.log(error);
         next(new Error(error));
       });
@@ -53,6 +63,5 @@ router.post('/mastery', (req, res, next) => {
     next(new Error('Invalid Summoner Name'));
   }
 });
-
 
 module.exports = router;
